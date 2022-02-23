a71b2056e1342d098eaecf2c1a2542e2d9f4eeef
Rabi León <rabi_leon@hotmail.com>
2022-02-23 08:05:19 -0600

    🚀 useCounter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { CounterApp } from './components/useState/CounterApp';
+import { CounterWithCustomHook } from './components/useState/CounterWithCustomHook';
 
 import { HookApp } from './HookApp';
 
-ReactDOM.render(<CounterApp />, document.getElementById('root'));
+ReactDOM.render(<CounterWithCustomHook />, document.getElementById('root'));
