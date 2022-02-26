ab6cee425599bebc9b5eda48e1272b2d028bdc25
Rabi León <rabi_leon@hotmail.com>
2022-02-26 15:06:19 -0600

    ⚠ warning useEffect

--- src/components/useEffect/Message.js
+++ src/components/useEffect/Message.js
@@ -1,17 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
+
   useEffect(() => {
     console.log('Compónente montado');
 
+    const mouseMove = (e) => {
+      console.log(e);
+      const coors = { x: e.x, y: e.y };
+      setCoords(coors);
+      // console.log(' :D');
+    };
+
+    window.addEventListener('mousemove', mouseMove);
+
     return () => {
       ('Componente desmontado');
+      window.removeEventListener('mousemove', mouseMove);
     };
   }, []);
 
   return (
     <div>
       <h3>Eres genial!</h3>
+      <p>
+        x: {x} y: {y}
+      </p>
     </div>
   );
 };
